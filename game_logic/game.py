e9ec2614acc88b69bf44f637306a7711b074d182
unknown <shayshn1234@gmail.com>
2025-10-23 12:44:04 +0300

    hope

---
+++ game_logic/game.py
@@ -0,0 +1,26 @@
+import utils.deck as f
+
+def create_player(name:str = "AI") -> dict:
+    thisdict = dict(player = name, hand = [], won_pile = [])
+    return thisdict
+
+
+def init_game()->dict:
+    p1 = create_player("shay")
+    p2 = create_player()
+    newdeck = f.create_deck()
+    f.shuffle(newdeck)
+    p1["hand"] = newdeck[:,26]
+    p2["hand"] = newdeck[26,:]
+    game_dict = dict(deck = newdeck, player_1 = p1, player_2 = p2)
+    return game_dict
+    
+    
+    
+    
+
+def play_round(p1:dict,p2:dict):
+    player1 = p1["hand"][0]
+    player2 = p2["hand"][0]
+    checking = f.compare_cards(player1, player2)
+    if checking == "p1"
---
+++ utils/deck.py
@@ -0,0 +1,44 @@
+import random
+
+def create_card(rank:str,suite:str) -> dict:
+    return dict(rank = rank, suite = suite, value = rank)
+
+def compare_cards(p1_card:dict, p2_card:dict) -> str:
+    p1 = p1_card.get("value")
+    p2 = p2_card.get("value")
+    if p1 > p2:
+        return str(p1)
+    if p1 < p2:
+        return str(p2)
+    if p1 == p2:
+        return "WAR"
+   
+          
+def create_deck() -> list[dict]:
+    rank = [i for i in range(2, 15)]
+    suite = [i for i in ("H","C","D","S")]
+    arr = []
+    
+    for r in rank:
+        for s in suite:
+                arr.append(create_card(r, s))
+    return arr
+
+
+def shuffle(deck:list[dict]) -> list[dict]:
+    temp = deck
+    while True:
+        for i in range(1000):
+            index1 = random.randrange(52)
+            index2 = random.randrange(52)
+            if index1 == index2:
+                continue
+            else:
+                temp[index1], temp[index2] = temp[index2], temp[index1]
+        return temp
+                   
+       
+
+
+
+
